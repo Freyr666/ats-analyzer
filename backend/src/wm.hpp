49d4004467f438d5f2184cde06888544ef25f460
Freyr <sky_rider_93@mail.ru>
2017-06-23 15:20:43 +0300

    minor changes

--- backend/src/wm.hpp
+++ backend/src/wm.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <gstreamermm.h>
+#include <map>
 
 #include "chatterer.hpp"
 #include "wm_config.hpp"
@@ -19,11 +20,14 @@ namespace Ats {
 	Wm(const Wm&) = delete;
 
 	void add_sink(uint stream, uint pid, Glib::RefPtr<Gst::Pad> sink);
+	void on_remove_sink(uint stream, uint pid);
 	void add_to_bin(Glib::RefPtr<Gst::Bin>);
 	Glib::RefPtr<Gst::Pad> get_src();
 
     private:
         Wm_config config;
+	std::map<std::pair<uint,uint>,Wm_window> windows;
+	std::map<std::pair<uint,uint>,Wm_widget> widgets;
 	Glib::RefPtr<Gst::Element> background;
 	Glib::RefPtr<Gst::Element> mixer;
 
--- backend/src/wm_config.hpp
+++ backend/src/wm_config.hpp
@@ -1,13 +1,39 @@
 #ifndef WM_CONFIG_H
 #define WM_CONFIG_H
 
+#include <map>
+#include <list>
+
 using namespace std;
 using namespace Glib;
 
 namespace Ats {
 
-    pair<uint,uint> resolution = make_pair(1920, 1080);
-    
+    class Wm_config {
+    public:
+	struct Widget {
+	    uint stream;
+	    uint pid;
+	    string name;
+	    pair<int,int> luc;
+	    pair<int,int> rlc;
+	};
+	struct Window {
+	    uint stream;
+	    uint pid;
+	    string name;
+	    pair<int,int> luc;
+	    pair<int,int> rlc;
+	    list<Widget>  childs;
+	};
+
+	
+    private:
+	pair<uint,uint> resolution = make_pair(1920, 1080);
+	map<pair<uint,uint>,Widget> avail_widgets;
+	map<pair<uint,uint>,Window> avail_windows;
+	list<Window> active_windows;
+    };
     
 }
 
