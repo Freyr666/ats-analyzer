fd05ce517b6ac70c8960d4ea24b0196e90bac4ce
Freyr <sky_rider_93@mail.ru>
2018-01-31 18:29:22 +0300

    initial chatterer implementation

--- backend/src/chatterer.rs
+++ backend/src/chatterer.rs
@@ -1,13 +1,143 @@
 use serde::{Serialize,Deserialize};
-use signals::Signal;
-use std::sync::{Arc,Mutex};
+//use signals::Signal;
+//use std::sync::{Arc,Mutex};
+use std::sync::mpsc::{Sender};
+use serde_json;
+use serde_msgpack;
+use std::str::FromStr;
+
+#[derive(Clone, Copy, Debug)]
+pub enum MsgType {
+    Msgpack,
+    Json
+}
+
+#[derive(Serialize)]
+struct Msg <'a, T: 'a> {
+    name: &'a str,
+    data: &'a T,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
-pub enum Response {
-    Fine,
+pub struct Name<'a> {
+    pub name: &'a str,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Request<'a, T: 'a> {
+    pub name: &'a str,
+    #[serde(bound(deserialize = "&'a T: Deserialize<'de>"))]
+    pub data: &'a T,
+}
+
+#[derive(Serialize, Debug)]
+pub enum Response<T> {
+    Fine (T),
     Error(String)
 }
 
+pub trait Addressable {
+    fn set_name (&mut self, String);
+    fn get_name (&self) -> &str;
+
+    fn set_format (&mut self, MsgType);
+    fn get_format (&self) -> MsgType;
+}
+
+pub trait Sendbox {
+    fn set_sender (&self, Sender<&[u8]>);
+    fn get_sender (&self) -> Option<&Sender<&[u8]>>;
+}
+
+pub trait Replybox<'a,T,R> {    
+    fn reply (&self, &'a T) -> Result<R,String>;    
+}
+
+pub trait Notifier<'a, T>: Addressable
+    where T: Serialize {
+    
+    fn serialize_msg (&'a self, data: &'a T) -> Vec<u8> {
+        let msg = Msg { name: self.get_name(),
+                        data };
+        match self.get_format() {
+            MsgType::Msgpack => serde_json::to_vec(&msg).unwrap(),
+            MsgType::Json    => serde_msgpack::to_vec(&msg).unwrap(),
+        }
+    }
+}
+
+pub trait Chatterer<'a, T>: Notifier<'a, T> + Sendbox
+    where T: Addressable + Serialize {
+    
+    fn connect_channel (&mut self, m: MsgType, s: Sender<&[u8]>) {
+        self.set_format(m);
+        self.set_sender(s);
+    }
+    
+    fn talk (&'a self, data: &'a T) {
+       let s = self.get_sender().unwrap();
+       s.send(&self.serialize_msg(data)).unwrap()
+    }
+}
+
+pub trait Respondent<'a, T, R>: Addressable + Replybox<'a,T,R>
+    where &'a T: 'a + Deserialize<'a>,
+          R: Serialize {
+
+    fn respond (&self, req: &'a [u8]) -> Vec<u8> {
+        let fmt = self.get_format();
+        
+        let v : Request<T> = match fmt {
+            MsgType::Msgpack => serde_msgpack::from_slice(&req).unwrap(),
+            MsgType::Json    => serde_json::from_slice(&req).unwrap(),
+        };
+        
+        if v.name != self.get_name() {
+            panic!("Respondent: name mismatch")
+        };
+
+        let rep = match self.reply(&v.data) {
+            Ok(v)  => Response::Fine(v),
+            Err(s) => Response::Error(s),
+        };
+
+        match fmt {
+            MsgType::Msgpack => serde_msgpack::to_vec(&rep).unwrap(),
+            MsgType::Json    => serde_json::to_vec(&rep).unwrap(),
+        }
+    }
+        
+}
+
+pub trait DispatchTable<'a> {
+    fn add_respondent (&mut self, String, Box<Fn(&'a [u8]) -> Vec<u8> + 'a>);
+    fn get_respondent (&'a self, &str) -> Option<&Box<Fn(&'a [u8]) -> Vec<u8> + 'a>>;
+}
+
+pub trait Dispatcher<'a>: Addressable + DispatchTable<'a> {
+
+    fn add_to_table<'b,T,R> (&'a mut self, r: &'a Respondent<'a,T,R>)
+        where &'a T: 'a + Deserialize<'a>,
+              R: Serialize {
+        let name = String::from_str(r.get_name()).unwrap();
+        //let clos = Box::new(move | buf | { r.respond(buf)});
+        self.add_respondent(name, Box::new(move | buf | { r.respond(buf)}) )
+    }
+    
+    fn dispatch (&'a self, r: &'a [u8]) -> Option<Vec<u8>> {
+        let n : Name = match self.get_format() {
+            MsgType::Msgpack => serde_msgpack::from_slice(&r).unwrap(),
+            MsgType::Json    => serde_json::from_slice(&r).unwrap(),
+        };
+
+        let resp = self.get_respondent(n.name)?;
+
+        Some(resp(r))
+    }
+    
+}
+
+/*
 pub trait Chatterer<'a,T>
     where T: Serialize + Deserialize<'a> {
 
@@ -17,3 +147,4 @@ pub trait Chatterer<'a,T>
     fn set_state(&self, b: &T) -> Response;
     fn signal(&self) -> Arc<Mutex<Signal<&'a T>>>;
 }
+*/
--- backend/src/context.rs
+++ backend/src/context.rs
@@ -10,26 +10,79 @@ use graph::GraphSettings;
 use preferences::Preferences;
 use std::thread;
 use serde_json;
+use serde_msgpack;
+use std::str::FromStr;
+use std::fmt::Display;
+use std::boxed::Box;
+use std::collections::HashMap;
+
+use chatterer::{MsgType,Name,Addressable,Dispatcher,DispatchTable};
 
 pub struct Context<'a> {
+    name:        String,
+    format:      MsgType,
+    table:       HashMap<String, (Box<Fn(&'a [u8]) -> Vec<u8> + 'a>)>,   
     probes:      Vec<Box<Probe>>,
     control:     Control,
     graph:       Graph<'a>,
     structure:   Structure,
     preferences: Preferences,
-    
 }
+
+impl<'a> Addressable for Context<'a> {
+    fn set_name (&mut self, s: String) {
+        self.name = s
+    }
+    fn get_name (&self) -> &str {
+        &self.name
+    }
+    fn set_format (&mut self, t: MsgType) {
+        self.format = t
+    }
+    fn get_format (&self) -> MsgType {
+        self.format
+    }
+}
+
+impl<'a> DispatchTable<'a> for Context<'a> {
+    fn add_respondent (&mut self, s: String, c: Box<Fn(&'a [u8]) -> Vec<u8> + 'a>) {
+        self.table.insert(s, c);
+    }
+    
+    fn get_respondent (&'a self, s: &str) -> Option<&Box<Fn(&'a [u8]) -> Vec<u8> + 'a>> {
+        self.table.get(s)
+    }
+}
+
+impl<'a> Dispatcher<'a> for Context<'a> {}
 
 impl<'a> Context<'a> {
     pub fn new(i : &Initial) -> Result<Context, String> {
+        let name        = String::from_str("context").unwrap();
+        let table       = HashMap::new();
+        let format      = MsgType::Json;
         let mut control = Control::new().unwrap();
         let structure   = Structure::new();
         let graph       = Graph::new().unwrap();
         let preferences = Preferences::new();
-        let s = serde_json::to_string(graph.settings).unwrap();
-        println!("Result: {}", s);
+        //let s  = serde_json::to_string(graph.settings).unwrap();
+        let js = "{\"name\": \"Hello\", \"val\": 42}";
+        let test : Vec<Box<Display>> = vec![Box::new(1), Box::new("string")];
+        //let msg = serde_msgpack::to_vec(serde_json::from_str(js).unwrap()).unwrap();
+        let v: Result<Name,_> = serde_json::from_str(js);
+        match v {
+            Ok(v) => println!("Result: {}", v.name),
+            Err(e) => println!("Error: {}", e),
+        };
+        for i in test {
+            println!("Val: {}", i);
+        }
+        
         control.connect(|s| { println!("String: {:?}", s); Vec::from("rval")} );
-        Ok(Context { probes: vec![],
+        Ok(Context { name,
+                     table,
+                     format,
+                     probes: vec![],
                      control,
                      structure,
                      graph,
