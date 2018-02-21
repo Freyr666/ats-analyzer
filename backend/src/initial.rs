7ff54bb7ffb3caff5b8cafc69ddf835ef009d4e8
alexander <sashayanin@gmail.com>
2018-02-21 17:53:43 +0300

    minor fixes

--- backend/src/initial.rs
+++ backend/src/initial.rs
@@ -34,14 +34,14 @@ pub enum UriError {
 }
 
 pub fn uri_error_to_string(e : &UriError) -> String {
-    match *e {
-        UriError::Prefix                => String::from("Bad prefix"),
-        UriError::OctetValue (v,o)      => format!("bad value ({}) in ip octet = {}", v, o + 1),
-        UriError::OctetMissing(o)       => format!("ip octet {} is missing", o + 1),
-        UriError::OctetNumber (o)       => format!("bad number ({}) of ip octets", o + 1),
-        UriError::BadSymbol(s)          => format!("bad symbol: {}",s),
-        UriError::PortValue(v)          => format!("bad port value ({})",v),
-        UriError::PortMissing           => String::from("port value missing after semicolon"),
+    match e {
+        &UriError::Prefix                  => String::from("Bad prefix"),
+        &UriError::OctetValue(ref v,ref o) => format!("bad value ({}) in ip octet = {}", v, o + 1),
+        &UriError::OctetMissing(ref o)     => format!("ip octet {} is missing", o + 1),
+        &UriError::OctetNumber (ref o)     => format!("bad number ({}) of ip octets", o + 1),
+        &UriError::BadSymbol(ref s)        => format!("bad symbol: {}",s),
+        &UriError::PortValue(ref v)        => format!("bad port value ({})",v),
+        &UriError::PortMissing             => String::from("port value missing after semicolon"),
     }
 }
 
@@ -51,75 +51,72 @@ pub fn check_ip_octet(v : i32, octet : i32) -> Result<i32, UriError> {
     else                     { return Ok (v) }
 }
 
-pub fn validate_uri(uri : &String) -> Result<&String, UriError> {
+pub fn validate_uri(uri : &str) -> Result<&str, UriError> {
 
     let prefix = "udp://";
-    if !uri.starts_with(prefix) {
-        return Err (UriError::Prefix)
-    }
-    else {
-        let trimmed = uri.trim_left_matches(prefix);
-        let mut state   : UriParserState = UriParserState::Ip;
-        let mut sym_cnt : u32 = 0;
-        let mut val     : i32 = 0;
-        let mut octet   : i32 = 0;
-        for sym in trimmed.chars() {
-            if sym >= '0' && sym <= '9' {
-                val     = (val * 10) + ((sym.to_digit(10).unwrap()) as i32);
-                sym_cnt = sym_cnt + 1;
+    if !uri.starts_with(prefix) { return Err (UriError::Prefix) }
+
+    let trimmed = uri.trim_left_matches(prefix);
+    let mut state   : UriParserState = UriParserState::Ip;
+    let mut sym_cnt : u32 = 0;
+    let mut val     : i32 = 0;
+    let mut octet   : i32 = 0;
+    for sym in trimmed.chars() {
+        if sym >= '0' && sym <= '9' {
+            val     = (val * 10) + ((sym.to_digit(10).unwrap()) as i32);
+            sym_cnt = sym_cnt + 1;
+        }
+        else if sym == '.' {
+            match state {
+                UriParserState::Ip =>
+                { if sym_cnt == 0 { return Err (UriError::BadSymbol(sym)) };
+                  match check_ip_octet(val,octet) {
+                      Ok (_)  => (),
+                      Err (e) => return Err (e),
+                  };
+                  octet   = octet + 1;
+                  val     = 0;
+                  sym_cnt = 0;
+                },
+                UriParserState::Port => return Err (UriError::BadSymbol(sym)),
             }
-            else if sym == '.' {
-                match state {
-                    UriParserState::Ip =>
-                    { if sym_cnt == 0 { return Err (UriError::BadSymbol(sym)) };
+        }
+        else if sym == ':' {
+            match state {
+                UriParserState::Ip =>
+                { if sym_cnt == 0    { return Err (UriError::BadSymbol(sym)) }
+                  else if octet != 3 { return Err (UriError::OctetNumber(octet)) }
+                  else {
                       match check_ip_octet(val,octet) {
                           Ok (_)  => (),
                           Err (e) => return Err (e),
                       };
-                      octet   = octet + 1;
                       val     = 0;
                       sym_cnt = 0;
-                    },
-                    UriParserState::Port => return Err (UriError::BadSymbol(sym)),
-                }
-            }
-            else if sym == ':' {
-                match state {
-                    UriParserState::Ip =>
-                    { if sym_cnt == 0    { return Err (UriError::BadSymbol(sym)) }
-                      else if octet != 3 { return Err (UriError::OctetNumber(octet)) }
-                      else {
-                          match check_ip_octet(val,octet) {
-                              Ok (_)  => (),
-                              Err (e) => return Err (e),
-                          };
-                          val     = 0;
-                          sym_cnt = 0;
-                          state   = UriParserState::Port;
-                      }
-                    },
-                    UriParserState::Port => { return Err (UriError::BadSymbol(sym)) },
-                }
-            }
-            else { return Err (UriError::BadSymbol(sym)) }
-        };
-        match state {
-            UriParserState::Ip   =>
-            { if sym_cnt == 0    { return Err (UriError::OctetMissing(octet)) }
-              else if octet != 3 { return Err (UriError::OctetNumber(octet)) }
-              else {
-                  match check_ip_octet(val,octet) {
-                      Ok (_)  => return Ok (uri),
-                      Err (e) => return Err (e),
+                      state   = UriParserState::Port;
                   }
-              }
+                },
+                UriParserState::Port => { return Err (UriError::BadSymbol(sym)) },
             }
-            UriParserState::Port =>
-            { if sym_cnt == 0                { return Err (UriError::PortMissing) }
-              else if val < 0 || val > 65535 { return Err (UriError::PortValue(val)) }
-              else                           { return Ok (uri) }
-            },
         }
+        else { return Err (UriError::BadSymbol(sym)) }
+    };
+    match state {
+        UriParserState::Ip   =>
+        { if sym_cnt == 0    { return Err (UriError::OctetMissing(octet)) }
+          else if octet != 3 { return Err (UriError::OctetNumber(octet)) }
+          else {
+              match check_ip_octet(val,octet) {
+                  Ok (_)  => return Ok (uri),
+                  Err (e) => return Err (e),
+              }
+          }
+        }
+        UriParserState::Port =>
+        { if sym_cnt == 0                { return Err (UriError::PortMissing) }
+          else if val < 0 || val > 65535 { return Err (UriError::PortValue(val)) }
+          else                           { return Ok (uri) }
+        },
     }
 }
 
@@ -149,8 +146,8 @@ impl Initial {
                     };
                 } else {
                     match validate_uri(&arg) {
-                        Ok (_)  => uris.push(arg),
-                        Err (e) =>
+                        Ok (uri) => uris.push(String::from(uri)),
+                        Err (e)  =>
                         { let e_str = uri_error_to_string(&e);
                           let s     = format!("{}: bad uri argument ({}), {}", path, arg, e_str);
                           return Err (Error::WrongOption(s))
