20e71115a0f39e15558095676c9437e5f5de9698
Freyr <sky_rider_93@mail.ru>
2018-05-31 16:45:44 +0300

    initial log facility impl

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,52 @@
+#[macro_use]
+extern crate log;
 extern crate backend;
+
+use log::{LevelFilter, Log, Record, Metadata};
 
 use backend::initial::{Initial,Error};
 use backend::context::Context;
 
+#[cfg(feature = "std")]
+use log::set_boxed_logger;
+
+#[cfg(not(feature = "std"))]
+fn set_boxed_logger(logger: Box<Log>) -> Result<(), log::SetLoggerError> {
+    log::set_logger(unsafe { &*Box::into_raw(logger) })
+}
+
+struct Logger;
+
+impl log::Log for Logger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        // metadata.level() <= Level::Info
+        true
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            println!("{} - {}", record.level(), record.args());
+        }
+    }
+
+    fn flush(&self) {}
+}
+
 fn main() {
+    let log_level_filter = match std::env::var("ATS3_LOG_LEVEL") {
+        Err (_) => LevelFilter::Off,
+        Ok(ref s) => match s.as_str() {
+            "error" => LevelFilter::Error,
+            "warn"  => LevelFilter::Warn,
+            "info"  => LevelFilter::Info,
+            "debug" => LevelFilter::Debug,
+            _ => LevelFilter::Off,
+        },
+    };
+    
+    set_boxed_logger(Box::new(Logger)).unwrap();
+    log::set_max_level(log_level_filter);
+    
     let args = std::env::args();
     let i    = match Initial::new(args) {
         Ok (i) => i,
