eae51d2723d3d4f8e47c9260aa214c9a62c3f9f6
Freyr666 <sky_rider_93@mail.ru>
2018-09-25 13:10:04 +0300

    socket fix

--- backend/src/control.rs
+++ backend/src/control.rs
@@ -19,8 +19,8 @@ impl Control {
         let (sender, receiver): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = channel();
         let received       = Arc::new(Mutex::new(Msg::new()));
         
-        in_socket.bind("ipc:///tmp/ats_qoe_in").unwrap();
-        out_socket.bind("ipc:///tmp/ats_qoe_out").unwrap();
+        in_socket.connect("ipc:///tmp/ats_qoe_in").unwrap();
+        out_socket.connect("ipc:///tmp/ats_qoe_out").unwrap();
         let r = received.clone();
         
         thread::Builder::new().name("recv_thread".to_string()).spawn(move || {
